c609c1879bbbe7ac5f1bcd16e81bfd68d4baf84d
MattCress99 <62275852+MattCress99@users.noreply.github.com>
2022-06-24 14:30:37 +0200

    removed /
    might be solution to the deploy error

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import FractalViewer from './components/pages/FractalViewer';
 
 const root = ReactDOM.createRoot( document.getElementById('root'));
-root.render(<App />);
+root.render(
+<App />);
 
