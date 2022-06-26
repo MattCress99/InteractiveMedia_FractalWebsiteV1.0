56aa7e2a1060d7b813ec0e456ae823a006f76de0
MattCress99 <62275852+MattCress99@users.noreply.github.com>
2022-06-27 00:16:21 +0200

    Revert "Updates"
    This reverts commit c047fae0d88889b7e74a9d3a184c49d9ab5dcdbe.

---
+++ src/components/pages/singleBlog/Blog3.jsx
@@ -0,0 +1,116 @@
+import React from 'react'
+import "./singleBlog1.css"
+
+export default function Blog3() {
+  return (
+    <div id="top" className="Blog3">
+            <h2>Internet, Society and Design Justice</h2>
+            <br></br>
+            <p>
+              Many individuals in today's society view the internet as a source
+              of change and growth. It is a place where new ideas can grow and
+              political and social movements can gain growth and followers. New
+              philosophies can be published, criticised and viewed throughout
+              the world. The internet has helped us all as a society to grow, it
+              has created worldwide awareness on subjects such as identity,
+              equality and inclusiveness. The internet is a powerful tool that
+              can be used for good and to spread awareness of global issues, for
+              instance, the current war happening between Russia and Ukraine.
+              Within minutes of Putin's invasion of Ukraine, a large majority of
+              the world was already aware of its happening. Due to the influence
+              of social media and news networks information spreads extremely
+              quickly across the web. This is great for helping to improve
+              global connectedness and communication, however, with factors such
+              as fake news, the after-effects of colonialism on decolonised
+              countries and the world's heavily invested reliance on the
+              'internet' there are a few things that must be considered when
+              viewing the internet and society. As a developer for these
+              platforms, one must always consider what is just in their design
+              as well as their design practices.
+            </p>
+            <br></br>
+            <p>
+              We are living in a far more progressive, inclusive and open-minded
+              society than ever before. Now more than ever we are seeing
+              individuals express themselves as who they wish to be, but in
+              order for everyone to feel included, especially on the internet,
+              it is important that we design with all individuals in mind.
+              Social constructs of gender and identity are slowly dissolving and
+              it is important for developers to make sure that no individual,
+              regardless of their identity, feels left out or injusticed due to
+              some design flaw. For instance Sasha Costanza-Chock describes
+              their troubling experiences with airport security in their book
+              'TravelingWhileTrans, Design Justice, and Escape from the Matrix
+              of Domination' [1]. They describes their experience of passing the
+              security gates at an airport, due to particular sociotechnical
+              configuration of gender normativity (cis-normativity, or the
+              assumption that all people have a gender identity that is
+              consistent with the sex they were assigned at birth) that has been
+              built into the scanner, The device is designed by social standards
+              to identify a person passing it as either male or female by the
+              genitalia it detects in the scan. There is no space for anything
+              inbetween and so it flagged Sasha, prompting them to a physical
+              patdown by a Transportation Security Administration (TSA) officer.
+              The next issue faced in this situation was the standard of who
+              would now search Sasha, as Sasha identifies as a nonbinary, trans,
+              femme-presenting person. The general standard is that individual
+              would be searched by officer of the same gender. In Sashas case,
+              neither of the TSA agents were of the same gender or indentified
+              as the same gender as her and so already Sasha was placed in a
+              position of embarrassment and humilition that they should not have
+              had to go through if these design features were taken into account
+              from the start.
+            </p>
+            <br></br>
+            <p>
+              When it comes to justful and inclusive design, designers must use
+              their designs to sustain, heal, and empower our communities, as
+              well as to seek liberation from exploitative and oppressive
+              systems[2]. It is our job as designers to creative inclusive
+              communities to make sure that no individual is left out or
+              unheard. This can be done through education and communication
+              which will always lead to greater awareness and hopefully cause
+              developers to think more about how they can design more user
+              inclusive and just designs globally. It is important that
+              developers understand and have a look at the effects of
+              colonisation and more importantly decolonisation when considering
+              their designs. Colonisation left many countries in states of
+              inequality and poverty and because of this it has taken much
+              longer for technology to catch up to them. These factors need to
+              be thought of by designers in order for their designs to be
+              utilized globally. We must consider the infrastructure and
+              technological capabilities that other areas in the world might
+              have, especially 3rd world countries. We need to think of ways
+              that our designs can be utilized effectively and can help promote
+              these 3rd world countries in order to improve their everyday
+              standard of living. As Designers it is our responsibility to
+              ensure a globally inclusive world, that accepts and includes all
+              individuals within it. By doing so we can help improve global
+              development, communication, education and improve the standard of
+              living for everyone within the world.
+            </p>
+            <p>By: Matthew Cresswell</p>
+            <br></br>
+            <h3>References:</h3>
+            <p>
+              [1] Costanza-Chock, S., 2020. Introduction: #TravelingWhileTrans,
+              Design Justice, and Escape from the Matrix of Domination. [online]
+              Design Justice. Available at:
+              "https://design-justice.pubpub.org/pub/ap8rgw5e/release/1"
+              [Accessed 21 April 2022].
+            </p>
+            <p>
+              [2]Design Justice Network. 2015. Design Justice Network. [online]
+              Available at: "https://designjustice.org/"" [Accessed 21 April
+              2022].
+            </p>
+            <p>
+              [3]Nations, U., 2022. Decolonization | United Nations. [online]
+              United Nations. Available at:
+              "https://www.un.org/en/global-issues/decolonization" [Accessed 21
+              April 2022].
+            </p>
+            <br></br>
+    </div>
+  )
+}
